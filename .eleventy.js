fd1e31f0b2135cc7d4362b1460deafe3f2e0ed46
Margaree Peacocke <margaree.p@gmail.com>
2021-04-09 15:58:46 -0400

    parse images

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
 /* global require, module */
 const cleanCSS = require('clean-css');
 const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 
+// TODO: imports
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('assets/js');
+	eleventyConfig.addPassthroughCopy('pages/components/imported/screenshots');
 
 	const options = {
 		html: false,
@@ -13,14 +15,18 @@ module.exports = function(eleventyConfig) {
 		modifyToken: (token) => {
 			// TODO: enable table cases after d2l-table is ready
 			switch (token.type) {
+				case 'image': {
+					const src = token.attrGet('src');
+					if (src.includes('../screenshots/'))
+						token.attrObj.src = src.replace(/..\/screenshots/, '/components/imported/screenshots');
+					else if (src.includes('./screenshots/'))
+						token.attrObj.src = src.replace(/.\/screenshots/, '/components/imported/screenshots');
+					break;
+				}
 				case 'link_open': {
 					token.tag = 'd2l-link';
 					const href = token.attrGet('href');
-					let newHref;
-					if (href.includes('.md')) newHref = href.replace(/.md/, '.html');
-					else if (href.includes('../screenshots/')) newHref = href.replace(/..\/screenshots/, '/components/imported/screenshots');
-					else if (href.includes('./screenshots/')) newHref = href.replace(/.\/screenshots/, '/components/imported/screenshots');
-					if (newHref) token.attrObj.href = newHref;
+					if (href.includes('.md')) token.attrObj.href = href.replace(/.md/, '.html');
 					break;
 				}
 				case 'link_close':
@@ -46,8 +52,9 @@ module.exports = function(eleventyConfig) {
 		if (content.includes('<!-- docs: live demo -->'))
 			return `<d2l-design-system-interactive-demo>${content}</d2l-design-system-interactive-demo>`;
 		else if (content.includes('<!-- docs: demo -->'))
-			return `<d2l-demo-snippet>${content}</d2l-demo-snippet>`
-		else return defaultFenceRule(tokens, idx, options, env, slf);
+			return `<d2l-demo-snippet>${content}</d2l-demo-snippet>`;
+		else
+			return defaultFenceRule(tokens, idx, options, env, slf);
 	};
 
 	const defaultTextRule = markdownIt.renderer.rules.text;
@@ -62,7 +69,8 @@ module.exports = function(eleventyConfig) {
 			const splitEnd = splitStart[1].split(' -->');
 			const contentClass = `d2l-cc-${splitEnd[0].replace(/ /g, '-')}`;
 			return `<div class="${contentClass}">`;
-		} else return defaultTextRule(tokens, idx, options, env, slf);
+		} else
+			return defaultTextRule(tokens, idx, options, env, slf);
 	};
 
 	eleventyConfig.addPlugin(eleventyNavigationPlugin);
