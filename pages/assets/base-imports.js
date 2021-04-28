d1b2b17e66c01354b93bb25500ace1d4f2a6af1d
Margaree Peacocke <margaree.p@gmail.com>
2021-04-28 13:27:11 -0400

    add navigation component

--- pages/assets/base-imports.js
+++ pages/assets/base-imports.js
@@ -1,11 +1,7 @@
-import { linkStyles } from '@brightspace-ui/core/components/link/link.js';
 import '@brightspace-ui/core/components/colors/colors.js';
+import '@brightspace-ui/core/components/link/link.js';
 import '@brightspace-ui/core/components/typography/typography.js';
-
-const styleSheet = document.createElement('style');
-styleSheet.type = 'text/css';
-styleSheet.innerText = linkStyles;
-document.head.appendChild(styleSheet);
+import './navigation.js';
 
 document.fonts.ready.then(() => {
 	document.body.classList.remove('d2l-component-catalog-body-unresolved'); // prevent FOUC
