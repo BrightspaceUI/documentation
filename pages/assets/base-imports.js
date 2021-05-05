4cc3c8a86125af210021678d492e0a439222b24f
Margaree Peacocke <margaree.p@gmail.com>
2021-05-05 15:38:28 -0400

    listen for DomContentLoaded instead of fonts ready

--- pages/assets/base-imports.js
+++ pages/assets/base-imports.js
@@ -2,7 +2,7 @@ import '@brightspace-ui/core/components/colors/colors.js';
 import '@brightspace-ui/core/components/link/link.js';
 import '@brightspace-ui/core/components/typography/typography.js';
 
-document.fonts.ready.then(() => {
+window.addEventListener('DOMContentLoaded', () => {
 	const elem = document.querySelector('div.d2l-component-catalog-content');
 	if (elem) elem.classList.remove('d2l-component-catalog-content-unresolved'); // prevent FOUC
 });
