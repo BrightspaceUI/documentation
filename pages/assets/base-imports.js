fca7518a02b683b959155c11e79bbbf0b7594e1c
Margaree Peacocke <margaree.p@gmail.com>
2021-05-05 14:45:35 -0400

    move content hiding class to be only on content since otherwise the sidebar flickers unnecessarily

--- pages/assets/base-imports.js
+++ pages/assets/base-imports.js
@@ -3,5 +3,6 @@ import '@brightspace-ui/core/components/link/link.js';
 import '@brightspace-ui/core/components/typography/typography.js';
 
 document.fonts.ready.then(() => {
-	document.body.classList.remove('d2l-component-catalog-body-unresolved'); // prevent FOUC
+	const elem = document.querySelector('div.d2l-component-catalog-content');
+	if (elem) elem.classList.remove('d2l-component-catalog-content-unresolved'); // prevent FOUC
 });
