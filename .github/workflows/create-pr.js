af8c7f387e0fe0f37562ff12ad9849fb5fa6a4cc
Margaree Peacocke <margaree.p@gmail.com>
2021-05-11 15:29:08 -0400

    initial work on github action that happens on issue label change

---
+++ .github/workflows/create-pr.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+const chalk = require('chalk');
+const { Octokit } = require('@octokit/rest');
+
+const [owner, repo] = process.env['GITHUB_REPOSITORY'].split('/');
+const branchName = process.env['BRANCH_NAME'];
+
+const octokit = new Octokit({
+	auth: process.env['GITHUB_TOKEN'],
+	baseUrl: process.env['GITHUB_API_URL'],
+	userAgent: `${process.env['GITHUB_WORKFLOW']}-update-component-pages`
+});
+
+// TODO: it would be nice to link to the issue(s) that changed and have that update if multiple changes are made
+async function handlePR() {
+	await octokit.request('POST /repos/{owner}/{repo}/pulls', {
+		owner: owner,
+		repo: repo,
+		title: 'Updating published component pages',
+		head: `refs/heads/${branchName}`,
+		base: `refs/heads/master`
+	});
+}
+
+handlePR().catch((e) => {
+	console.log(chalk.red(e));
+	console.log(chalk.red('Could not open new PR'));
+});
