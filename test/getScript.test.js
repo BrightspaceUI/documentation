78febe03dbf00a5cf9ed0581fb35f7492032dad5
George Plukov <george.plukov@d2l.com>
2021-05-27 15:19:57 -0400

    Adda couple tests for get Scripts

---
+++ test/getScript.test.js
@@ -0,0 +1,24 @@
+const { expectation } = require('sinon');
+const assert = require('assert');
+const sinon = require('sinon');
+const { getScript } = require('../tools/getScript');
+
+const exampleScript1 = `<!-- docs: code demo -->
+<script type="module">
+  import '@brightspace-ui/core/components/button/button.js';
+</script>
+<d2l-button $attributes>Button</d2l-button>`;
+
+describe('getScript', () => {
+	// todo add tests for getScript
+	it ('Returns default imports', () => {
+		const imports = getScript(exampleScript1);
+
+		assert(imports.includes('typography.js'));
+	});
+	it ('appends ?module to script imports', () => {
+		const imports = getScript(exampleScript1);
+
+		assert(imports.includes('button.js?module'));
+	});
+});
